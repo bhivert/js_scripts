7964dbcf68de0e34b8bf8f2a682180e6b294aeda
bhivert <hivert.benoit@gmail.com>
2017-10-05 16:11:57 +0200

    generic collapse update

--- collapse.js
+++ collapse.js
@@ -1,10 +1,10 @@
-$('a').click(function(){
+$('a.unique-collapse').click(function(){
 	var target = $(this).attr('data-target');
 	var r;
 	var	target_id = ((r = /^#(.*) .collapse$/.exec(target)) != null) ? r[1] : null;
 
 	if (target_id) {
-		$('#cv .collapse').each(function(){
+		$('.collapse').each(function(){
 			if ($(this).parent().attr('id') != target_id)
 				$(this).removeClass('in');
 		});
